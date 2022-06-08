5ffa036c3adaa94b71d24ec48107ccdcc9d201a7
andreoneres <andreoliveiraneres@gmail.com>
2022-06-07 21:42:48 -0300

    feat: page title

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,18 +2,25 @@ import { useLanguageContext } from '../contexts/language';
 import { Header } from '../components/header';
 import { Home } from '../components/home';
 import { Container, Main } from '../components/layout/styles';
+import Head from 'next/head';
 
 function Index() {
   const context = useLanguageContext();
   console.log(context);
 
   return (
-    <Container>
-      <Header />
-      <Main>
-        <Home />
-      </Main>
-    </Container>
+    <>
+      <Head>
+        <title>André Oliveira</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Container>
+        <Header />
+        <Main>
+          <Home />
+        </Main>
+      </Container>
+    </>
   );
 }
 
